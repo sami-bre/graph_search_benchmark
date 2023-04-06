ef8de010fc7b66c9ce7cb948d26543651a1f1d74
sami-bre <samuelbirhanu121@gmail.com>
2023-04-06 19:56:30 +0300

    write some test code for the aStar search method in the Graph class

--- test_graph.py
+++ test_graph.py
@@ -1,4 +1,5 @@
 from graph import Graph
+from heuristic import haversine_distance
 
 graph = Graph()
 
@@ -93,3 +94,68 @@ graph.deleteUndirectedEdge('a', 'b')
 \n""")
 
 graph.show()
+
+
+print("=================== Building a new graph ====================")
+
+
+romania = Graph()
+
+romania.addNode("Arad")
+romania.addNode("Zerind")
+romania.addNode("Sibiu")
+romania.addNode("Timisoara")
+romania.addNode("Oradea")
+romania.addNode("Fagaras")
+romania.addNode("Rimnicu Vilcea")
+romania.addNode("Pitesti")
+romania.addNode("Bucharest")
+
+print("""\n
+after:
+romania.addNode("Arad")
+romania.addNode("Zerind")
+romania.addNode("Sibiu")
+romania.addNode("Timisoara")
+romania.addNode("Oradea")
+romania.addNode("Fagaras")
+romania.addNode("Rimnicu Vilcea")
+romania.addNode("Pitesti")
+romania.addNode("Bucharest")
+\n""")
+      
+graph.show()
+
+romania.addUndirectedEdge("Arad", "Zerind", 75, 75)
+romania.addUndirectedEdge("Arad", "Timisoara", 118, 118)
+romania.addUndirectedEdge("Zerind", "Oradea", 71, 71)
+romania.addUndirectedEdge("Arad", "Sibiu", 140, 140)
+romania.addUndirectedEdge("Oradea", "Sibiu", 151, 151)
+romania.addUndirectedEdge("Sibiu", "Fagaras", 99, 99)
+romania.addUndirectedEdge("Sibiu", "Rimnicu Vilcea", 80, 80)
+romania.addUndirectedEdge("Rimnicu Vilcea", "Pitesti", 97, 97)
+romania.addUndirectedEdge("Pitesti", "Bucharest", 101, 101)
+romania.addUndirectedEdge("Fagaras", "Bucharest", 211, 211)
+
+
+print("""\n
+after:
+graph.addUndirectedEdge("Arad", "Zerind", 75, 75)
+graph.addUndirectedEdge("Arad", "Timisoara", 118, 118)
+graph.addUndirectedEdge("Zerind", "Oradea", 71, 71)
+graph.addUndirectedEdge("Arad", "Sibiu", 140, 140)
+graph.addUndirectedEdge("Oradea", "Sibiu", 151, 151)
+graph.addUndirectedEdge("Sibiu", "Fagaras", 99, 99)
+graph.addUndirectedEdge("Sibiu", "Rimnicu Vilcea", 80, 80)
+graph.addUndirectedEdge("Rimnicu Vilcea", "Pitesti", 97, 97)
+graph.addUndirectedEdge("Pitesti", "Bucharest", 101, 101)
+graph.addUndirectedEdge("Fagaras", "Bucharest", 211, 211)
+\n""")
+
+
+romania.show()
+
+
+print("result of doing aStar search from Arad to Bucharest:")
+result = romania.aStarSearch("Arad", "Bucharest", heuristic=haversine_distance)
+print(result)
